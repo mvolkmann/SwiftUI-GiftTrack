5f4558a2f464f78167d03e2a61b9c1cca9159b47
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-10 05:35:45 -0600

    Initial Commit

---
+++ gift-track/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gift-track
+//
+//  Created by R. Mark Volkmann on 11/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gift-track/gift_trackApp.swift
@@ -0,0 +1,17 @@
+//
+//  gift_trackApp.swift
+//  gift-track
+//
+//  Created by R. Mark Volkmann on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct gift_trackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
