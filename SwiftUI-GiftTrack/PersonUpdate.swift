36e0c2bb13df3344213e9a1cb3d72ab79bd13fcd
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-18 08:40:54 -0600

    progress

--- SwiftUI-GiftTrack/PersonUpdate.swift
+++ SwiftUI-GiftTrack/PersonUpdate.swift
@@ -4,14 +4,14 @@ struct PersonUpdate: View {
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var vm: ViewModel
     
-    @State var name = ""
-    @State var birthday = Date.now
+    @State private var name = ""
+    @State private var birthday = Date.now
     
     var person: PersonEntity
     
     init(person: PersonEntity) {
         self.person = person
-        //TODO: Why do I need so much syntax to do this?
+        // TODO: Why do I need so much syntax to do this?
         self._name = State(
             initialValue: person.value(forKey: "name") as? String ?? ""
         )
@@ -27,23 +27,29 @@ struct PersonUpdate: View {
     var body: some View {
         Form {
             TextField("Name", text: $name)
+            /* to persist immediately ...
+                .onChange(of: name) { newName in
+                    person.name = newName
+                    vm.saveContext()
+                    vm.fetchPeople()
+                }
+             */
             DatePicker(
                 "Birthday",
                 selection: $birthday,
                 displayedComponents: .date
             )
             ControlGroup {
-                //TODO: Try to persist changes as they are made
-                //TODO: so an Update button isn't needed.
-                Button("Update") {
-                    //TODO: Why don't these changes make it back to PeopleView?
+                // TODO: Try to persist changes as they are made
+                // TODO: so an Update button isn't needed.
+                Button("Done") {
                     person.name = name
                     person.birthday = birthday
                     print("person =", person)
                     vm.saveContext()
                     
-                    //TODO: Can I update a single person in the ViewModel
-                    //TODO: without refetching all of them again?
+                    // TODO: Can I update a single person in the ViewModel
+                    // TODO: without refetching all of them again?
                     vm.fetchPeople()
                     back()
                 }
